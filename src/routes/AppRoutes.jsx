b99426516dd111461e85df07315276300e1482b5
Jose Alvarez Dominguez <josealvarezdominguez00@gmail.com>
2025-04-24 20:55:55 -0300

    create and update footer in different pages

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,4 @@
-import { Routes, Route } from "react-router-dom";
-import { useLocation, Navigate } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { useEffect } from "react";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
@@ -10,22 +9,29 @@ import { Toaster } from "react-hot-toast";
 
 function AppRoutes() {
   const location = useLocation();
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
+
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Toaster />
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/carrito" element={<Carrito />} />
-        <Route path="/pedido" element={<Pedido />} />
-        {/* <Route path="/error" element={<Error />} />
-        <Route path="*" element={<Navigate to="/error" replace />} /> */}
-      </Routes>
+
+      {/* El main se expande para empujar el footer abajo */}
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/carrito" element={<Carrito />} />
+          <Route path="/pedido" element={<Pedido />} />
+          {/* <Route path="/error" element={<Error />} />
+          <Route path="*" element={<Navigate to="/error" replace />} /> */}
+        </Routes>
+      </main>
+
       <Footer />
-    </>
+    </div>
   );
 }
 
